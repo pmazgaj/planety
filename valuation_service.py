871a8448f0778349b959d1e3ec2221c6f3601a06
pmazgaj <pmazgaj@mazgaj.pl>
2017-05-10 16:42:24 +0200

    Word

--- valuation_service.py
+++ valuation_service.py
@@ -1,49 +1,71 @@
 """
-Module for validation service
+File for importing csv directly into models.
+Currently - for shop and category models.
 """
+import csv
+import sys
+import os
+import pprint as pp
+
+__author__ = "Przemek"
+
+PROJECT_PATH = os.path.realpath(os.getcwd())
+CSV_PATH = os.path.join(PROJECT_PATH, 'csv')
 
 
-def num_in_range(num: int) -> bool:
-    """return True if number in converted (int) number, 1 <= n < m < 10000, else False"""
-    return True if num in range(1, 10001) else False
-
-
-def get_num() -> int:
+def convert_currency() -> int:
     """
-    Check if number is valid for int conversion and in range, if is - convert and return
-    :return: integer number
+
+    :return: converted integer number
     """
-    number = None
-    while number is None:
-        try:
-            number = int(input('Type a number\n'))
-            while not num_in_range(number):
-                number = int(input('Type a number\n'))
-        except ValueError:
-            print('Not valid integer number')
-    return number
+    return int
 
 
-def generate_fizz_buzz():
-    """print strings according to specification"""
-    number_start = get_num()
-    number_end = get_num()
-    # print(number_start, number_end)
-    # add 1 (inclusive range)
-    for number in range(number_start, number_end + 1):
-        if number % 5 == 0 and number % 3 == 0:
-            print('FizzBuzz')
-        elif number % 5 == 0:
-            print('Buzz')
-        elif number % 3 == 0:
-            print('Fizz')
-        else:
-            print(number)
+def save_csv():
+    """
+    Save results to file top_products
+    :return:
+    """
+    with open(os.path.join(CSV_PATH, 'top_products.csv'), mode='w+'):
+        print('writing')
+
+
+def parse_reader(reader: csv.DictReader):
+    """
+    Return list of all data sets in file
+    :param reader:
+    :return:
+    """
+    return [row for row in reader]
+
+
+def parse_csv(file: str) -> csv.DictReader:
+    """
+    Open csv file, return dict for file
+    :param file: file, with extension
+    :return: dict describing file (e.g. column_name: column}
+    """
+
+    with open(os.path.join(CSV_PATH, file)) as file:
+        reader = csv.DictReader(file)
+        parsed_data = parse_reader(reader)
+        return parsed_data
 
 
 def main():
-    """Handle entire module"""
-    generate_fizz_buzz()
+    """
+    Handle entire module
+    :return:
+    """
+    currencies = parse_csv('currencies.csv')
+    matches = parse_csv('matchings.csv')
+    data = parse_csv('data.csv')
+
+    # currencies = parse_reader(currencies)
+    # matches = parse_reader(matches)
+    # data = parse_reader(data)
+
+    print(currencies)
 
 
 if __name__ == "__main__":
